a5311c8bb46a79f6d136b23c2d3cadeed53530a5
Raffael Tancman <rtancman@gmail.com>
2018-10-11 13:47:45 -0300

    feat: Create AddEntry Component

---
+++ components/AddEntry.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default class AddEntry extends Component {
+  render() {
+    return (
+      <View>
+        {getMetricMetaInfo('run').getIcon()}
+        {getMetricMetaInfo('bike').getIcon()}
+      </View>
+    )
+  }
+}
