29655fe1d20b85b999de98949853a9fcf41eb88c
Raffael Tancman <rtancman@gmail.com>
2018-10-11 21:46:47 -0300

    feat: Create TextButton component and add template login view

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import TextButton from './TextButton'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
@@ -63,8 +65,30 @@ export default class AddEntry extends Component {
     // Clear local notification
   }
 
+  reset = () => {
+    const key = timeToString()
+    // Update Redux
+    // Route to Home
+    // Update "DB"
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name={'ios-happy-outline'}
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
