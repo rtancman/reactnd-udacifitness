d34a9251ce97ae0df1a67bdd1b78dfd8de0ffe36
Raffael Tancman <rtancman@gmail.com>
2018-10-15 12:59:36 -0300

    feat: Changes for History

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     )
