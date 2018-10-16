3c0e8f6647592716762a9282184730c108204eb0
Raffael Tancman <rtancman@gmail.com>
2018-10-16 12:42:47 -0300

    feat: Changes for Live-denied

--- components/Live.js
+++ components/Live.js
@@ -7,7 +7,7 @@ export default class Live extends Component {
   state = {
     coords: null,
     status: null,
-    status: 'undetermined',
+    status: 'denied',
     direction: ''
   }
 
@@ -23,8 +23,11 @@ export default class Live extends Component {
 
     if (status === 'denied') {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling location services for this app.
+          </Text>
         </View>
       )
     }
